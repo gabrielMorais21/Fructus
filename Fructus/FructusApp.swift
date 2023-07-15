e0fc859d81d89b29ef96bec76db4751cb01e1cd7
gabriel.dias <gabriel.dias@granitopagamentos.com.br>
2023-07-14 23:23:44 -0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Gabriel Morais Dias on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
